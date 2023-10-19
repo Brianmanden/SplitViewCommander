681d00573e237fd362db6da312f9ef54b60bd197
Brian Andersen <brianmanden@gmail.com>
2023-10-19 23:05:03 +0200

    ADD Experimenting with gui-cs

--- SplitViewCommander/Program.cs
+++ SplitViewCommander/Program.cs
@@ -1,13 +1,67 @@
 ﻿using SplitViewCommander;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 using Terminal.Gui;
 
 Application.Init();
+var win = new Window("Hello")
+{
+    X = 0,
+    Y = 1,
+    Width = Dim.Fill(),
+    Height = Dim.Fill(),
+};
 
-try
+#region DataTable
+var dt = new DataTable();
+using (var con = new SqlConnection("server=(localdb)\\MSSQLLocalDb; database = BooksDB; integrated security = true; encrypt = false;"))
 {
-    Application.Run(new MyView());
+    con.Open();
+    var cmd = new SqlCommand("SELECT * FROM Books;", con);
+    var adapter = new SqlDataAdapter(cmd);
+
+    adapter.Fill(dt);
 }
-finally
+
+var tableView = new TableView()
 {
-    Application.Shutdown();
-}
+    X = 30,
+    Y = 20,
+    Width = 150,
+    Height = 10,
+};
+tableView.Table = dt;
+#endregion
+
+string theNow = DateTime.Now.ToString("HH:mm:ss");
+
+var menu = new MenuBar(new MenuBarItem[] {
+            new MenuBarItem (
+                "_File", new MenuItem [] {
+                    new MenuItem ("_Quit", "", () => { Application.RequestStop(); })
+            }),
+            new MenuBarItem (
+                "_Table", new MenuItem [] {
+                    new MenuItem ("_Add Table", "", () => { win.Add(tableView); }),
+                    new MenuItem ("_Close Table", "", () => { win.Remove(tableView); }),
+            }),
+            new MenuBarItem (
+                "_Mark", new MenuItem [] {
+                    new MenuItem ("_Quit", "", () => { Application.RequestStop(); })
+            }),
+            new MenuBarItem (
+                "_Options", new MenuItem [] {
+                    new MenuItem ("_Quit", "", () => { Application.RequestStop(); })
+            }),
+        });
+
+
+
+// Add both menu and win in a single call
+//Application.Top.Add(menu, win, tableView);
+Application.Top.Add(menu, win);
+Application.Run();
+Application.Shutdown();
+
+
