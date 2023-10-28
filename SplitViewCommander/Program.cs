b45339ccaeb1376b40e259a0a9e2ac17ef6e230c
Brian Andersen <brianmanden@gmail.com>
2023-10-28 16:05:41 +0200

    UPDATE ListView - can now navigate directories WIP

--- SplitViewCommander/Program.cs
+++ SplitViewCommander/Program.cs
@@ -1,8 +1,9 @@
-﻿using SplitViewCommander;
-using System;
+﻿using SplitViewCommander.Services;
 using System.Data;
 using System.Data.SqlClient;
 using Terminal.Gui;
+
+SvcUtils utils = new SvcUtils();
 
 Application.Init();
 var win = new Window("Hello")
@@ -35,36 +36,60 @@ tableView.Table = dt;
 #endregion
 
 var menu = new MenuBar(new MenuBarItem[] {
-            new MenuBarItem (
-                "_File", new MenuItem [] {
-                    new MenuItem ("_Quit", "", () => { Application.RequestStop(); })
-            }),
-            new MenuBarItem (
-                "_Table", new MenuItem [] {
-                    new MenuItem ("_Add Table", "", () => { win.Add(tableView); }),
-                    new MenuItem ("_Close Table", "", () => { win.Remove(tableView); }),
-            }),
-            new MenuBarItem (
-                "_Mark", new MenuItem [] {
-                    new MenuItem ("_Quit", "", () => { Application.RequestStop(); })
-            }),
-            new MenuBarItem (
-                "_Options", new MenuItem [] {
-                    new MenuItem ("_Quit", "", () => { Application.RequestStop(); })
-            }),
+            new MenuBarItem ( "_File", new MenuItem [] { new MenuItem ("_Quit", "", () => { Application.RequestStop(); })            }),
+            new MenuBarItem ( "_Table", new MenuItem [] { new MenuItem ("_Add Table", "", () => { win.Add(tableView); }), new MenuItem ("_Close Table", "", () => { win.Remove(tableView); }),}),
+            new MenuBarItem ( "_Mark", new MenuItem [] { new MenuItem ("_Quit", "", () => { Application.RequestStop(); }) }),
+            new MenuBarItem ( "_Options", new MenuItem [] { new MenuItem ("_Quit", "", () => { Application.RequestStop(); }) }),
         });
 
 var listSource  = new List<string>{ "123", "324", "123", "324", "123", "324", };
-var listView = new ListView(listSource)
+
+string currentLeftDir = @"C:\Users\Brian\Desktop";
+string currentRightDir = Directory.GetCurrentDirectory().ToString();
+
+string[] relDirRefs = new string[] { ".", ".." };
+string[] dirFileList = Directory.GetFileSystemEntries(currentLeftDir, "*", SearchOption.TopDirectoryOnly);
+string[] files1 = utils.ConcatArrays(relDirRefs, dirFileList);
+
+// Border WIP
+//Border listViewBorder = new Border {
+//    Background = Color.BrightCyan,
+//    BorderBrush = Color.Red,
+//    BorderStyle = BorderStyle.Single,
+//    BorderThickness = new Thickness(13),
+//    DrawMarginFrame = true
+//};
+
+var listView = new ListView(files1){ Width = Dim.Percent(45), Height = Dim.Percent(45), X = Pos.Percent(0), Y = Pos.Percent(0), AllowsMarking = true, AllowsMultipleSelection = true };
+var listView2 = new ListView() { Width = Dim.Percent(45), Height = Dim.Percent(45), X = Pos.Percent(51), Y = Pos.Percent(0), AllowsMultipleSelection = true };
+listView.OpenSelectedItem += HandleOpenSelectedItem;
+
+void HandleOpenSelectedItem(ListViewItemEventArgs args)
 {
-    Width = 100,
-    Height = 100,
-    X = 50,
-    Y = 40,
-};
-listView.AllowsMultipleSelection = true;
+    string filePath = args.Value.ToString();
+    bool isDir = utils.IsDirectory(filePath);
+
+    if (isDir)
+    {
+        if (".." == filePath)
+        {
+            currentLeftDir = Directory.GetParent(currentLeftDir).ToString();
+        }
+        else
+        {
+            currentLeftDir = filePath;
+        }
+    }
+
+    string[] dirsAndFilesAsList        = Directory.EnumerateFileSystemEntries(currentLeftDir).ToArray();
+    string[] entries = Directory.GetFileSystemEntries(currentLeftDir, "*", SearchOption.TopDirectoryOnly);
+    
+    listView.Clear();
+    listView.SetSource(utils.ConcatArrays(relDirRefs, dirsAndFilesAsList));
+}
 
 win.Add(listView);
+win.Add(listView2);
 
 // Add both menu and win in a single call
 //Application.Top.Add(menu, win, tableView);
