1f1e85e8525c127e893efbe2185e40d850ac07d7
Brian Andersen <brianmanden@gmail.com>
2023-11-02 18:27:37 +0100

    ADD dummy buttons

--- SplitViewCommander/Program.cs
+++ SplitViewCommander/Program.cs
@@ -16,6 +16,7 @@ MenuBar menu = new MenuBar(new MenuBarItem[] {
             new MenuBarItem ( "_Options", new MenuItem [] { new MenuItem ("_Quit", "", () => { Application.RequestStop(); }) }),
         });
 
+#region Directory Panels
 string currentLeftDir = @"C:\Users\Brian\Desktop";
 string currentRightDir = Directory.GetCurrentDirectory().ToString();
 
@@ -25,10 +26,11 @@ string[] leftFiles = SvcUtils.ConcatArrays(relativeDirectoryReferences, leftDirF
 string[] rightDirFileList = Directory.GetFileSystemEntries(currentLeftDir, "*", SearchOption.TopDirectoryOnly);
 string[] rightFiles = SvcUtils.ConcatArrays(relativeDirectoryReferences, rightDirFileList);
 
-ListView leftListView = new ListView(leftFiles){ Width = Dim.Percent(45), Height = Dim.Percent(45), X = Pos.Percent(0), Y = Pos.Percent(0), AllowsMarking = true, AllowsMultipleSelection = true };
+ListView leftListView = new ListView(leftFiles) { Width = Dim.Percent(45), Height = Dim.Percent(45), X = Pos.Percent(0), Y = Pos.Percent(0), AllowsMarking = true, AllowsMultipleSelection = true };
 ListView rightListView = new ListView(rightFiles) { Width = Dim.Percent(45), Height = Dim.Percent(45), X = Pos.Percent(51), Y = Pos.Percent(0), AllowsMarking = true, AllowsMultipleSelection = true };
 leftListView.OpenSelectedItem += LeftListViewHandleOpenSelectedItem;
 rightListView.OpenSelectedItem += RightListViewHandleOpenSelectedItem;
+#endregion
 
 void LeftListViewHandleOpenSelectedItem(ListViewItemEventArgs args)
 {
@@ -98,22 +100,30 @@ void RightListViewHandleOpenSelectedItem(ListViewItemEventArgs args)
 win.Add(leftListView);
 win.Add(rightListView);
 
+Button f3Button = new Button(10, 25, "_F3 View", false);
+f3Button.CanFocus = false;
 
+Button f4Button = new Button(25, 25, "_F4 Edit", false);
+f4Button.CanFocus = false;
 
-Button f3Button = new Button(10, 25, "F3 View", false);
-Button f4Button = new Button(25, 25, "F4 Edit", false);
-Button f10Button = new Button(40, 25, "_F10 Quit", false);
+Button f5Button = new Button(40, 25, "_F5 Copy", false);
+f5Button.CanFocus = false;
+
+Button f6Button = new Button(55, 25, "_F6 Move", false);
+f6Button.CanFocus = false;
+
+Button f7Button = new Button(70, 25, "_F7 Dir", false);
+f7Button.CanFocus = false;
+
+Button f10Button = new Button(85, 25, "_F10 Quit", false);
+f10Button.CanFocus = false;
 
 win.Add(f3Button);
 win.Add(f4Button);
+win.Add(f5Button);
+win.Add(f6Button);
+win.Add(f7Button);
 win.Add(f10Button);
-
-
-
-StatusBar statusBar = new StatusBar();
-win.Add(statusBar);
-
-
 
 Application.Top.KeyDown += OnKeyDown;
 void OnKeyDown(View.KeyEventEventArgs args)
@@ -127,9 +137,7 @@ void OnKeyDown(View.KeyEventEventArgs args)
 }
 
 
-
 // Add both menu and win in a single call
-//Application.Top.Add(menu, win, tableView);
 Application.Top.Add(menu, win);
 Application.Run();
 Application.Shutdown();
